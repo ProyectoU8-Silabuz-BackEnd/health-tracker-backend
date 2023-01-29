544beee371ffe20ff2c4fa55ca6634625c0427aa
KevinSucapuca <ksucapuca007@gmail.com>
2023-01-29 18:34:24 -0500

    add new pastilla

--- src/components/pastillas/controller.ts
+++ src/components/pastillas/controller.ts
@@ -18,19 +18,29 @@ export const allpastillas = async (_req: Request, res: Response): Promise<void> 
     }
 
 
-    export const findpastillas = async (req:Request, res: Response): Promise<void> => {
+export const findpastillas = async (req:Request, res: Response): Promise<void> => {
 
-        try { const {id} = req.params;
-            const idpastilla= await prisma.pastillas.findUnique({ where:{
-                id: Number(id),
-            },});
+    try { const {id} = req.params;
+        const idpastilla= await prisma.pastillas.findUnique({ where:{
+            id: Number(id),
+        },});
 
-            res.status(200).json({
-                ok:true,
-                data : idpastilla,
-            })
-            } catch (error) {
-                res.status(500).json({
-                    ok:false,message:error});
-                }
+        res.status(200).json({
+            ok:true,
+            data : idpastilla,
+        })
+        } catch (error) {
+            res.status(500).json({
+                ok:false,message:error});
+            }
+        }
+            
+
+export const addpastilla= async(req:Request, res: Response): Promise<void> =>{
+    try {
+        const {id_enfermedad,  dosis} = req.body;
+
+        await prisma.pastillas.create({
+            data:{
+                id_enfermedad: Number(id_enfermedad),
             }
