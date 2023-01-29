432c4b9fc49259926664713ed2f1293badf20c11
KevinSucapuca <ksucapuca007@gmail.com>
2023-01-29 18:10:31 -0500

    add find allpastillas  controller pastillas

--- src/components/pastillas/controller.ts
+++ src/components/pastillas/controller.ts
@@ -1,3 +1,18 @@
 import type { Request, Response} from "express";
 import { PrismaClient } from "@prisma/client";
 
+const prisma= new PrismaClient();
+
+export const allpastillas = async (_req: Request, res: Response): Promise<void> => {
+    try {
+        const pastillas= await prisma.pastillas.findMany();
+        res.status(200).json({
+            ok:true,
+            data : pastillas,
+
+        });
+    } catch (error) {
+        res.status(500).json({
+            ok:false,message:error});
+        }
+    }
