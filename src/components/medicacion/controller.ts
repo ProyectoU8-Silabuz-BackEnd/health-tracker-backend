6e7e96db72139bced58ecda7b0bbc4b481af7728
braulio <braulio.escobar@pucp.edu.pe>
2023-01-29 16:30:50 -0500

    findAll findOne methods

---
+++ src/components/medicacion/controller.ts
@@ -0,0 +1,46 @@
+import type { Request, Response} from "express";
+import { PrismaClient } from "@prisma/client";
+
+const prisma= new PrismaClient();
+
+export const medicacionFindAll = async (_req: Request, res: Response): Promise<void> => {
+    try {
+        const medicaciones = await prisma.medicacion.findMany();
+
+        res.status(200).json({
+            ok: true,
+            data: medicaciones,
+        });
+
+    } catch (error) {
+        res.status(500).json({ ok: false, message: error });
+    }
+}
+
+export const medicacionFindOne = async (req:Request, res: Response): Promise<void> => {
+
+    try {
+    	
+    	const {pacienteId, pastillaId} = req.params;
+
+        const medicacion = await prisma.medicacion.findUnique({
+        	where:{
+		        pacienteId: Number(pacienteId),
+		        pastillaId: Number(pastillaId),
+		    },
+		});
+
+        res.status(200).json({
+            ok: true,
+            data: medicacion,
+		})
+
+    } catch (error) {
+
+        res.status(500).json({
+            ok:false,
+            message: error,
+        })
+        
+    }
+}
