be31116f03211f0701ae659776ccf22cef8968ba
renato <renatocespedesf@gmail.com>
2023-01-30 16:01:16 -0500

    add route for method http post

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,8 @@
 import express, { type Application } from "express";
-import { recordatoryRouter } from "./components";
-import { medicacionRouter } from "./components";
-import { inventarioRouter } from "./components";
-import { enfermedadRouter } from "./components";
+import { loginrouter, recordatoryRouter,
+        medicacionRouter,inventarioRouter,
+        enfermedadRouter } from "./components";
+
 
 const app: Application = express();
 app.use(express.json());
@@ -12,5 +12,5 @@ app.use("/api/v1/recordatory", recordatoryRouter);
 app.use("/api/v1/medicacion", medicacionRouter);
 app.use("/api/v1/inventario", inventarioRouter);
 app.use("/api/v1/enfermedad", enfermedadRouter);
-
+app.use("/api/v1/user/login", loginrouter);
 export default app;
