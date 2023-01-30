5438e919dacb942777b5abc894824f237129f2bb
zanaborio <zanaborio21@gmail.com>
2023-01-29 21:29:20 -0500

    Arreglado y agregado el componente enfermedad

---
+++ src/components/pastillas/controller.ts
@@ -0,0 +1,62 @@
+import type { Request, Response} from "express";
+import { PrismaClient } from "@prisma/client";
+
+const prisma= new PrismaClient();
+
+export const allpastillas = async (_req: Request, res: Response): Promise<void> => {
+    try {
+        const pastillas= await prisma.pastillas.findMany();
+        res.status(200).json({
+            ok:true,
+            data : pastillas,
+
+        });
+    } catch (error) {
+        res.status(500).json({
+            ok:false,message:error});
+        }
+    }
+
+
+export const findpastillas = async (req:Request, res: Response): Promise<void> => {
+
+    try { const {id} = req.params;
+        const idpastilla= await prisma.pastillas.findUnique({ where:{
+            id: Number(id),
+        },});
+
+        res.status(200).json({
+            ok:true,
+            data : idpastilla,
+        })
+        } catch (error) {
+            res.status(500).json({
+                ok:false,message:error});
+            }
+        }
+            
+
+export const addpastilla= async(req:Request, res: Response): Promise<void> =>{
+    try {
+        const {id_enfermedad,nombre,  dosis} = req.body;
+
+        await prisma.pastillas.create({
+            data:{
+                id_enfermedad: id_enfermedad,
+                nombre: nombre,
+                dosis:dosis,
+            }
+        });
+
+        res.status(200).json({
+            ok:true,message:"Pastilla agredada correctamente"
+        });
+
+    } catch (error) {
+        res.status(500).json({
+
+            ok:false,message:error
+
+        });
+    }
+}
