7a1934865f3e1f72618d0ebe1585ad41bdbdb40c
Aljes2310 <aljes2310e@gmail.com>
2023-02-02 17:21:52 -0500

    twilio con mensajes programados en base a fechas

--- src/services/twilio/controller.ts
+++ src/services/twilio/controller.ts
@@ -1,5 +1,6 @@
 import type { Request, Response} from "express";
 import twilio from 'twilio';
+import { getDateArray } from "./funciones";
 
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
@@ -7,24 +8,52 @@ const service = process.env.TWILIO_MESSAGING_SERVICE_SID;
 
 const client = twilio(accountSid, authToken);
 
+const client = twilio(accountSid, authToken);
+
 export const sendMessage= async (req:Request,res:Response)=>{
-    const {number,message}=req.body;
-    const createMessage=await client.messages.create({
+    const {number,message, from, to, interval}=req.body;
+
+    let fechainicial = new Date(from);
+    let fechafinal = new Date(to);
+    let intervalo_milisegundos= Number(interval)*1000*60*60;
+    let datearray= getDateArray(fechainicial, fechafinal, intervalo_milisegundos);
+    console.log(datearray[0]);
+            //interval : milisegundos
+
+    try {    
+    for (let i = 0; i < datearray.length; i++) {
+    await client.messages.create({
         body:message,
         messagingServiceSid:service,
         to: number,
+        sendAt: datearray[i],
+        scheduleType: 'fixed',
     });
-    console.log(createMessage);
-    // console.log(createMessage.id);
-    if(!createMessage){
+     } res.status(201).json({
+        ok:true,
+        message: "mensajes creados"
+     })}
+
+    catch(error) {
         res.status(401).json({
             ok:false,
             message: "Algo salio mal"
         })
     }
-    res.status(200).json({
-        ok:true,
-        message:"El mensaje se envio con exito"
-    })
-}
+   }
+      
+
+
+
+/* 
+Verify that the SendAt parameter is using "[YYYY]-[MM]-[DD]T[HH]:[MM]:[SS]Z" format (in UTC). */
+
+
+/* {
+    "number":"+51929076660",
+    "message":"mensaje enviado desde twilio",
+    "from":"2023-02-02T19:00:00Z",
+    "to": "2023-02-02T23:00:00Z" ,
+    "interval" : "3"
+} */
 
