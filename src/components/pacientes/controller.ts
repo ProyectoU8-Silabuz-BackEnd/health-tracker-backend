f4b7ce9a2cc137df1b2eb497fd2f1571f2faa062
renato <renatocespedesf@gmail.com>
2023-02-03 14:14:24 -0500

    modify addpaciente function accordint to the fields in db

--- src/components/pacientes/controller.ts
+++ src/components/pacientes/controller.ts
@@ -1,11 +1,15 @@
 import type { Request, Response} from "express";
 import { PrismaClient } from "@prisma/client";
-
+import {User} from "./../../interfaces/userInterface"
 const prisma= new PrismaClient();
 
 export const allpacientes = async (_req: Request, res: Response): Promise<void> => {
     try {
-        const pacientes = await prisma.pacients.findMany();
+        const pacientes = await prisma.pacients.findMany({
+            include:{
+                users:true
+            }
+        });
 
         res.status(200).json({
             ok: true,
@@ -21,9 +25,14 @@ export const allpacientes = async (_req: Request, res: Response): Promise<void> 
 export const findpaciente = async (req:Request, res: Response): Promise<void> => {
 
     try { const {id} = req.params;
-        const pacienteid = await prisma.pacients.findUnique({where: {
-        id: Number(id),
-                },});
+        const pacienteid = await prisma.pacients.findUnique({
+            where: {
+                id: Number(id),
+                },
+            include:{
+                users:true
+            }
+            });
         res.status(200).json({
             ok:true,
             data:pacienteid ,
@@ -41,16 +50,36 @@ export const findpaciente = async (req:Request, res: Response): Promise<void> =>
 
 export const addpaciente= async(req:Request, res: Response): Promise<void> =>{
     try {
-        const data = req.body;
-
-        await prisma.pacients.create({
-            data,
+        const {nombre,dni,edad,celular,users} = req.body;
+        var role;
+        console.log(users);
+        const pacient=await prisma.pacients.create({
+            data:{
+                nombre:nombre,
+                dni:dni,
+                edad:edad,
+                celular:celular,
+                users: {
+                    connectOrCreate:{
+                        where:{
+                            correo:users.correo
+                        },
+                        create:{
+                            correo:users.correo,
+                            password:users.password,
+                            rol:"pacient"
+                        },
+                    },
+                },
+            },
             include:{
                 users:true
             }
         });
                 res.status(201).json({
-                    ok:true, message: "Paciente añadido correctamente"
+                    ok:true, 
+                    message: "Paciente añadido correctamente",
+                    pacient:pacient
                 });
 
             }
