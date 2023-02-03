c71abd322cf0f099adf3ee73cccc96bf14128fca
renato <renatocespedesf@gmail.com>
2023-02-03 17:44:56 -0500

    create function to get one recordatory

--- src/components/recordatory/controller.ts
+++ src/components/recordatory/controller.ts
@@ -1,5 +1,6 @@
 import type { Request, Response} from "express";
 import { PrismaClient } from "@prisma/client";
+import { Connect } from "twilio/lib/twiml/VoiceResponse";
 
 
 const prisma= new PrismaClient();
@@ -8,8 +9,13 @@ const prisma= new PrismaClient();
 
 export const findAll = async (_req: Request, res: Response): Promise<void> => {
     try {
-        const recordatory = await prisma.recordatory.findMany();
-        console.log(recordatory);
+        const recordatory = await prisma.recordatory.findMany({
+            include:{
+                medicamento:true,
+                pacientes:true
+            }
+        });
+
         res.status(200).json({
             ok: true,
             data: recordatory,
@@ -20,13 +26,54 @@ export const findAll = async (_req: Request, res: Response): Promise<void> => {
 
 }
 
+
+export const findRecordatory = async (req:Request, res: Response): Promise<void> => {
+
+    try { const {id} = req.params;
+        const recordatory = await prisma.recordatory.findUnique({
+            where: {
+                id: Number(id),
+                },
+            include:{
+                pacientes:true,
+                medicamento:true
+            }
+            });
+        res.status(200).json({
+            ok:true,
+            data:recordatory ,
+        })
+
+    } catch (error) {
+        res.status(500).json({
+            ok:false,
+            message: error,
+        })
+        
+    }
+}
+
+
 export const create= async (req:Request, res: Response): Promise<void> =>{
     console.log(req.body);
     try {
-        const data = req.body;
-        console.log("test");
+        const {Fecha_inicio,
+            Fecha_fin,
+            interval,
+            message,
+            medicamento,
+            pacientes} = req.body;
         // console.log(paciente_id,medicacion_id,metodo_notificacion,Hora);
-        await prisma.recordatory.create({data});
+        await prisma.recordatory.create({
+            data:{
+                Fecha_inicio:Fecha_inicio,
+                Fecha_fin:Fecha_fin,
+                interval:interval,
+                message:message,
+                medicamento:{connect:{id:medicamento}},
+                pacientes:{connect:{id:pacientes}}
+            }
+        });
         res.status(201).json({
             ok:true, message: "Recordatorio creado correctamente"
         });
